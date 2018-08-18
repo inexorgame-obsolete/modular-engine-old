2ab7e6e25a39c7cde04409c4114a8f089d3bc645
a_teammate <madoe3@web.de>
2018-08-18 16:51:03 +0200

    use default constructor for module2

--- module2/module2.cpp
+++ module2/module2.cpp
@@ -9,9 +9,7 @@ private:
 	Module1* module1;
 
 public:
-    INJECT(Module2Impl(Module1* module1))
-        : module1(module1) {
-    }
+    INJECT(Module2Impl()) = default;
 
     virtual void doSomething() override {
     	module1->write("Hello world!\n");
@@ -21,5 +19,5 @@ public:
 
 fruit::Component<Module2> getModule2Component() {
     return fruit::createComponent()
-            .bind<Module2, Module2Impl>();
+            .install(getModule1Component);
 }
