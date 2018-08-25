4b8a91ae4616903c2f26d4ce9ae8d5b95c2037e6
a_teammate <madoe3@web.de>
2018-08-25 20:17:00 +0200

    fix 2ndlast commit (some more files changed)

--- logger/spdlogger/src/spd_logger.hpp
+++ logger/spdlogger/src/spd_logger.hpp
@@ -7,26 +7,7 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
 
-#undef LOG_INFO  //conflicting between spdlog and cef
-#undef LOG_WARNING
-
-#include <array>
-#include <iomanip>
-#include <map>
-#include <memory>                                           // for shared_ptr
-#include <ostream>                                          // for operator<<
-#include <string>                                           // for string
-#include <vector>
-
-#include <array>
-#include <iomanip>
-#include <map>
-#include <memory>                                           // for shared_ptr
-#include <ostream>                                          // for operator<<
-#include <string>                                           // for string
-#include <vector>
-
-#include "logger.hpp"
+#include "logger/logger.hpp"
 
 namespace inexor {
 namespace util {
